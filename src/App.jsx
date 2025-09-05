4013b62cda4f1d4c19ea18cb56c2b9226024ca3f
UsamaShafique218 <usamashafique218@gmail.com>
2025-09-05 16:23:45 +0500

    Initial commit with Vite + Tailwind setup

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,9 @@ function App() {
 
   return (
     <>
-      Conva Dashboard
+      <div className="flex h-screen items-center justify-center bg-red-900">
+        <h1 className="text-4xl font-bold text-white">aaaa</h1>
+      </div>
     </>
   )
 }
