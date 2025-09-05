fa4b469e7039043f7295018b9d5df7d72b9feba6
UsamaShafique218 <usamashafique218@gmail.com>
2025-09-05 17:57:29 +0500

    Initial commit - React Vite setup

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
       <div className="flex h-screen items-center justify-center bg-red-900">
-        <h1 className="text-4xl font-bold text-white">aaaa</h1>
+        <h1 className="text-4xl font-bold text-white">Conva Dashbaord</h1>
       </div>
     </>
   )
